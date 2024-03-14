c50fdb43734a6580fc2ee4c15f5df047cb99763b
Shivam Gupta <64886313+SGCODEX@users.noreply.github.com>
2024-03-14 13:34:47 +0530

    Added files

---
+++ coding-assistant-codellama-streamlit/app.py
@@ -0,0 +1,39 @@
+from utils import *
+
+
+
+# app config
+st.set_page_config(page_title="✨E Cell DTC Code Generator - CodyBot✨", page_icon="✨🤖✨")
+st.title("🤖 CodyBot - Code Generator at E Cell-DTC, Your Coding Assistant! ✨")
+st.markdown("© 2024 E Cell-DTC. All rights reserved.")
+
+
+# Setting generation configuration - This will override the parameter that is set in the Modelfile
+get_config_gen = configure_generation()
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I am CodyBot - Code Generator at E Cell-DTC, Your Coding Assistant!. How can I help you?"),
+    ]
+
+for message in st.session_state.chat_history:
+    with st.chat_message("AI" if isinstance(message, AIMessage) else "Human"):
+        st.write(message.content)
+
+# user input
+if user_query := st.chat_input("Type your message here..."):
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    with st.chat_message("Human"):
+        st.write(user_query)
+
+    with st.chat_message("AI"):
+        response_placeholder = st.empty()
+        full_response = ""
+        for chunk in get_realtime_response(user_prompt=user_query, **get_config_gen):
+            full_response += chunk
+            response_placeholder.write(full_response)
+        logger.info("Response successfully generated.")
+    
+    st.session_state.chat_history.append(AIMessage(content=full_response))
+
+
